bfaf04f502c2dd872fed0364df53bbe6375e05b9
Yoruki <48972421+yoruki1104@users.noreply.github.com>
2019-12-26 10:57:27 +0800

    Add files via upload

---
+++ js/scrip.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+  // Load JSON file
+  let openingAnimWindow = document.querySelector('#openingLottie');
+  let openingAnimData = {
+    container: openingAnimWindow,
+    animType: 'svg',
+    loop: true,
+    prerender: true,
+    autoplay: true,
+    // path: 'json/download-icon.json'
+    path: 'https://labs.nearpod.com/bodymovin/demo/promo/data.json'
+  };
+  // set bodymovin
+  let openingAnim = bodymovin.loadAnimation(openingAnimData);
+
+});
+  
